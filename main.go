63dc53fac0ef20f319f9e17e372662ee744f82d1
Tracy <tmullen@clearblade.com>
2018-05-19 13:11:19 -0500

    mutation

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"math/rand"
 	"net/http"
+	"time"
 
 	"github.com/graphql-go/graphql"
 	"github.com/graphql-go/handler"
@@ -10,14 +12,34 @@ import (
 )
 
 type Person struct {
-	ID   int64  `json:"id"`
+	ID   string `json:"id"`
 	Name string `json:"name"`
-	From string `json:"from"`
+}
+
+var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+func RandStringRunes(n int) string {
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letterRunes[rand.Intn(len(letterRunes))]
+	}
+	return string(b)
+}
+
+var PersonList []Person
+
+func init() {
+	person1 := Person{ID: "a", Name: "Jenny"}
+	person2 := Person{ID: "b", Name: "Henery"}
+	person3 := Person{ID: "b", Name: "Abagle"}
+	PersonList = append(PersonList, person1, person2, person3)
+
+	rand.Seed(time.Now().UnixNano())
 }
 
 // var People []Person
 func getPeople() []Person {
-	results := []Person{Person{1, "Jenny", "false"}, Person{2, "Henry", "true"}}
+	results := PersonList
 	return results
 }
 
@@ -26,12 +48,9 @@ var personType = graphql.NewObject(
 		Name: "Person",
 		Fields: graphql.Fields{
 			"id": &graphql.Field{
-				Type: graphql.Int,
-			},
-			"name": &graphql.Field{
 				Type: graphql.String,
 			},
-			"from": &graphql.Field{
+			"name": &graphql.Field{
 				Type: graphql.String,
 			},
 		},
@@ -44,14 +63,55 @@ var queryType = graphql.NewObject(graphql.ObjectConfig{
 			Type:        graphql.NewList(personType),
 			Description: "List of people",
 			Resolve: func(p graphql.ResolveParams) (interface{}, error) {
-				// People = getPeople()
 				return getPeople(), nil
 			},
 		},
 	},
 })
+
+// root mutation
+var rootMutation = graphql.NewObject(graphql.ObjectConfig{
+	Name: "RootMutation",
+	Fields: graphql.Fields{
+		/*
+			curl -g 'http://localhost:8080/graphql?query=mutation+_{createPerson(text:"My+new+person"){id,text,done}}'
+		*/
+		"createPerson": &graphql.Field{
+			Type:        personType, // the return type for this field
+			Description: "Create new person",
+			Args: graphql.FieldConfigArgument{
+				"name": &graphql.ArgumentConfig{
+					Type: graphql.NewNonNull(graphql.String),
+				},
+			},
+			Resolve: func(params graphql.ResolveParams) (interface{}, error) {
+
+				// marshall and cast the argument value
+				name, _ := params.Args["name"].(string)
+
+				// perform mutation operation here
+				// for e.g. create a Person and save to DB.
+				newPerson := Person{
+					ID:   RandStringRunes(8),
+					Name: name,
+				}
+
+				PersonList = append(PersonList, newPerson)
+
+				// return the new Person object that we supposedly save to DB
+				// Note here that
+				// - we are returning a `Person` struct instance here
+				// - we previously specified the return Type to be `personType`
+				// - `Person` struct maps to `personType`, as defined in `personType` ObjectConfig`
+				return newPerson, nil
+			},
+		},
+	},
+})
+
 var Schema, _ = graphql.NewSchema(graphql.SchemaConfig{
-	Query: queryType,
+	Query:    queryType,
+	Mutation: rootMutation,
 })
 
 func main() {
@@ -69,3 +129,17 @@ func main() {
 	serveMux.HandleFunc("/graphiql", graphiql.ServeGraphiQL)
 	http.ListenAndServe(":8080", serveMux)
 }
+
+// query {
+// 	People {
+// 	  name
+// 	  id
+// 	}
+//   }
+
+// mutation {
+// 	createPerson(name: "Lenny") {
+// 	  name
+// 	  id
+// 	}
+//   }
