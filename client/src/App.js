aef6f802eef975e9e21a750b409676f504a7dd9b
Tracy <tmullen@clearblade.com>
2018-05-20 12:16:40 -0500

    images show overlayed

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,31 @@
 import React from "react";
 import { gql } from "apollo-boost";
 import { Query } from "react-apollo";
+import { random } from "lodash";
 
-const Image = ({ url, size }) => {
-  return <img src={url} />;
+const OverlayedImages = ({ images }) => {
+  return (
+    <div
+      style={{
+        width: "100%",
+        height: "100%",
+        position: "absolute",
+        top: 0,
+        background: images.map(im => `url("${im.url}")`).join(","),
+        backgroundBlendMode: "darken",
+        backgroundPositionX: images
+          .map(im => `${im.size * random(50, 150)}px`)
+          .join(", "),
+        backgroundPositionY: images
+          .map(im => `${im.size * random(50, 150)}px`)
+          .join(", "),
+        backgroundSize: images
+          .map(im => `${im.size * random(50, 150)}px`)
+          .join(", ")
+      }}
+      alt={images[0].category}
+    />
+  );
 };
 
 const GET_IMAGES = gql`
@@ -11,6 +33,7 @@ const GET_IMAGES = gql`
     SrcImagesRandom {
       id
       url
+      size
       category
     }
   }
@@ -22,7 +45,11 @@ const App = () => (
       if (loading) return <div>Loading...</div>;
       if (error) return <div>Error :(</div>;
 
-      return <div>{data.SrcImagesRandom.map(d => <Image {...d} />)}</div>;
+      return (
+        <div>
+          <OverlayedImages images={data.SrcImagesRandom} />)}
+        </div>
+      );
     }}
   </Query>
 );
